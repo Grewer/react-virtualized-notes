e7d4b80a4c439795600efae5edf72b83b8757b57
grewer <grewer@grewer.cn>
2022-01-29 23:23:46 +0800

    feat: 开篇, 介绍和 demo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# react-virtualized-notes 源码阅读
+# react-virtual-notes 源码阅读
 
+> 前言: 这次本来想解析 react-virtualized 的源码, 但是他的内容太多, 太杂, 我们先从小的库入手, 由点及面
+> 所以这次改为了 react-virtual 和 react-window 的源码, 这篇就是 react-virtual
+
+
+
+
+## 引用
+
+- https://react-virtual.tanstack.com/docs/overview 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import './App.css';
+import React from "react";
+import {useVirtual} from "react-virtual";
 
 
 function App(props) {
+    const parentRef = React.useRef()
 
-    console.log('App1', props)
+    const rowVirtualizer = useVirtual({
+        size: 10000,
+        parentRef,
+        estimateSize: React.useCallback(() => 35, []),
+    })
 
     return (
-        <div>111</div>
-    );
+        <>
+            <div
+                ref={parentRef}
+                className="List"
+                style={{
+                    height: `150px`,
+                    width: `300px`,
+                    overflow: 'auto',
+                }}
+            >
+                <div
+                    className="ListInner"
+                    style={{
+                        height: `${rowVirtualizer.totalSize}px`,
+                        width: '100%',
+                        position: 'relative',
+                    }}
+                >
+                    {rowVirtualizer.virtualItems.map(virtualRow => (
+                        <div
+                            key={virtualRow.index}
+                            className={virtualRow.index % 2 ? 'ListItemOdd' : 'ListItemEven'}
+                            style={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                width: '100%',
+                                height: `${virtualRow.size}px`,
+                                transform: `translateY(${virtualRow.start}px)`,
+                            }}
+                        >
+                            Row {virtualRow.index}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 }
 
 
