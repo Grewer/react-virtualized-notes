9776f8322286c613628402e9e0dcb10ece2f98b2
grewer <grewer@grewer.cn>
2022-02-12 23:57:26 +0800

    feat: react-window 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import React from "react";
 import {useVirtual} from "./react-virtual";
+import { FixedSizeList as List } from 'react-window';
 
+const Row = ({ index, style }) => (
+    <div style={style}>Row {index}</div>
+);
 
-function App(props) {
+const App = () => (
+    <List
+        height={150}
+        itemCount={1000}
+        itemSize={35}
+        width={300}
+    >
+        {Row}
+    </List>
+);
+
+function App2(props) {
     const parentRef = React.useRef()
 
     const rowVirtualizer = useVirtual({
